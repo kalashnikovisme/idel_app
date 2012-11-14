9ce91414dee5557c7d18e520a01c18a8f4cc4360
kalashnikovisme <kalashnikovisme@gmail.com>
2012-11-14 05:59:50 +0400

    idel_app

--- idel_app/Form1.Designer.cs
+++ idel_app/Form1.Designer.cs
@@ -30,7 +30,7 @@
       // 
       this.button1.Location = new System.Drawing.Point(50, 68);
       this.button1.Name = "button1";
-      this.button1.Size = new System.Drawing.Size(75, 23);
+      this.button1.Size = new System.Drawing.Size(100, 82);
       this.button1.TabIndex = 0;
       this.button1.Text = "button1";
       this.button1.UseVisualStyleBackColor = true;
