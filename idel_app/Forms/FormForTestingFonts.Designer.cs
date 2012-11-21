67410b5cca28bba22a60156a22e6eb10dc20ebb8
kalashnikovisme <kalashnikovisme@gmail.com>
2012-11-22 00:15:52 +0400

    back up

---
+++ idel_app/Forms/FormForTestingFonts.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace idel_app.Forms
+{
+    partial class FormForTestingFonts
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.run = new System.Windows.Forms.Button();
+            this.buttonText = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // run
+            // 
+            this.run.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.run.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.run.ForeColor = System.Drawing.Color.Red;
+            this.run.Location = new System.Drawing.Point(689, 258);
+            this.run.Name = "run";
+            this.run.Size = new System.Drawing.Size(149, 100);
+            this.run.TabIndex = 7;
+            this.run.Text = "Resize Font (Жми сюда)";
+            this.run.UseVisualStyleBackColor = true;
+            this.run.Click += new System.EventHandler(this.run_Click);
+            // 
+            // buttonText
+            // 
+            this.buttonText.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonText.Location = new System.Drawing.Point(12, 12);
+            this.buttonText.Name = "buttonText";
+            this.buttonText.Size = new System.Drawing.Size(78, 240);
+            this.buttonText.TabIndex = 8;
+            this.buttonText.Text = "1234$^*$^%AA";
+            this.buttonText.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(96, 83);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(373, 275);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "1234$^*$^%AA";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(475, 83);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(96, 275);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "1234$^*$^%AA";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(96, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(742, 65);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "1234$^*$^%AA";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(577, 83);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(261, 32);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "1234$^*$^%AA";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button5.Location = new System.Drawing.Point(577, 121);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(80, 32);
+            this.button5.TabIndex = 13;
+            this.button5.Text = "1234$^*$^%AA";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button6.Location = new System.Drawing.Point(663, 121);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(175, 32);
+            this.button6.TabIndex = 14;
+            this.button6.Text = "1234$^*$^%AA";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button7.Location = new System.Drawing.Point(577, 159);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(46, 199);
+            this.button7.TabIndex = 15;
+            this.button7.Text = "1234$^*$^%AA";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button8.Location = new System.Drawing.Point(629, 159);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(46, 122);
+            this.button8.TabIndex = 16;
+            this.button8.Text = "1234$^*$^%AA";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button9.Location = new System.Drawing.Point(681, 159);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(46, 90);
+            this.button9.TabIndex = 17;
+            this.button9.Text = "1234$^*$^%AA";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(850, 370);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonText);
+            this.Controls.Add(this.run);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button run;
+        private System.Windows.Forms.Button buttonText;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+    }
+}
---
+++ upgradeControls/upgradeControls/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace upgradeControls
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void resizeFont(Button b)
+        {
+            const int minFontSize = 7;
+            //определение подходящего шрифта по ширине control'a
+            float neededSizeX = (b.Width - b.Font.Size * 2) / b.Text.Length;
+            int tempSizeXInPoint = minFontSize;
+            while (true)
+            {
+                Graphics g = this.CreateGraphics();
+                Font tempFont = new Font(b.Font.FontFamily, tempSizeXInPoint);
+                int tempSize = (int)g.MeasureString(b.Text, tempFont).Width / b.Text.Length;
+                if (neededSizeX - tempSize < 0.5)
+                {
+                    break;
+                }
+                tempSizeXInPoint++;
+                #region forDebug
+                if (tempSizeXInPoint == 300)
+                {
+                    MessageBox.Show("while true in X");
+                    break;
+                }
+                #endregion
+            }
+            //определение подходящего шрифта по высоте control'a
+            int tempSizeYInPoint = minFontSize;
+            while (true)
+            {
+                Graphics g = this.CreateGraphics();
+                Font tempFont = new Font(b.Font.FontFamily, tempSizeYInPoint);
+                if (b.Height - g.MeasureString(b.Text, tempFont).Height < 5)
+                {
+                    break;
+                }
+                tempSizeYInPoint++;
+                #region forDebug
+                if (tempSizeYInPoint == 300)
+                {
+                    MessageBox.Show("while true in Y");
+                    break;
+                }
+                #endregion
+            }
+            //выбор наиболее подходящего из двух найденых шрифтов
+            int resSize;
+            Font resFont;
+            if (tempSizeYInPoint > tempSizeXInPoint) // выбираем минимальный размер шрифта, т.к. главное что бы отобразилось все содержимое
+            {
+                resSize = tempSizeXInPoint;//размер определенный по ширине подошел
+                resFont = new Font(b.Font.FontFamily, resSize);
+                int numberOfLines = (int)((b.Height - 10) / resFont.GetHeight());//есть ли возможность разбить содержимое на несколько строк 
+                if (numberOfLines > 1)
+                {
+                    int tempResSize = resSize;
+                    while (true)
+                    {
+                        Graphics g = this.CreateGraphics();
+                        Font tempFont = new Font(b.Font.FontFamily, tempResSize);
+                        numberOfLines = (int)((b.Height - 10) / tempFont.GetHeight());
+                        if ((b.Width - tempResSize * 2) * numberOfLines - g.MeasureString(b.Text, tempFont).Width < 100)
+                        {
+                            break;
+                        }
+                        tempResSize++;
+                        #region forDebug
+                        if (tempResSize == 300)
+                        {
+                            MessageBox.Show("while true in Res");
+                            break;
+                        }
+                        #endregion
+
+                    }
+                    //подходящий размер шрифта определен, теперь проверяем не слишком ли он мал
+                    if (tempResSize < minFontSize + 2)
+                    {
+                        resFont = new Font(b.Font.FontFamily, minFontSize);
+                    }
+                    else if (tempResSize < 25)
+                    {
+                        resFont = new Font(b.Font.FontFamily, tempResSize - 1);
+                    }
+                    else // -1,-2 расходуется на погрешности отступов от краев control'a до текста
+                    {
+                        resFont = new Font(b.Font.FontFamily, tempResSize - 2);
+                    }
+                }
+
+            }
+            else
+            {
+                resSize = tempSizeYInPoint; //размер определенный по высоте подошел
+                resFont = new Font(b.Font.FontFamily, resSize);
+            }
+            b.Font = resFont;
+        }
+
+
+        //Старая версия лежит "на всякий пожарный"
+        private void resizeFontOldVersion(Button b)
+        {
+            float neededSizeX = (b.Width - b.Font.Size * 2) / b.Text.Length;
+            int tempSizeXInPoint = 8;
+            while (true)
+            {
+                Font tempFont = null;
+                Graphics g = this.CreateGraphics();
+                tempFont = new Font(b.Font.FontFamily, tempSizeXInPoint);
+                //int accuracy = (tempSizeXInPoint > 40) ? (int)(tempSizeXInPoint * b.Text.Length * 0.6) : 10;
+                int tempSize = (int)g.MeasureString(b.Text, tempFont).Width / b.Text.Length;
+                if (neededSizeX - tempSize < 0.5)
+                {
+                    break;
+                }
+                if (tempSizeXInPoint == 300)
+                {
+                    MessageBox.Show("while true in X");
+                    break;
+                }
+                tempSizeXInPoint++;
+            }
+            int tempSizeYInPoint = 8;
+            while (true)
+            {
+                Font tempFont = null;
+                Graphics g = this.CreateGraphics();
+                tempFont = new Font(b.Font.FontFamily, tempSizeYInPoint);
+                if (b.Height - g.MeasureString(b.Text, tempFont).Height < 5)
+                {
+                    break;
+                }
+                if (tempSizeYInPoint == 300)
+                {
+                    MessageBox.Show("while true in Y");
+                    break;
+                }
+                tempSizeYInPoint++;
+            }
+            int resSize;
+            Font resFont;
+            if (tempSizeYInPoint > tempSizeXInPoint)
+            {
+                resSize = tempSizeXInPoint;
+                resFont = new Font(b.Font.FontFamily, resSize);
+                int numberOfLines = (int)((b.Height - 10) / resFont.GetHeight());
+                if (numberOfLines > 1)
+                {
+                    int tempResSize = resSize;
+                    while (true)
+                    {
+                        Font tempFont = null;
+                        Graphics g = this.CreateGraphics();
+                        tempFont = new Font(b.Font.FontFamily, tempResSize);
+                        numberOfLines = (int)((b.Height - 10) / tempFont.GetHeight());
+                        if ((b.Width - tempResSize * 2) * numberOfLines - g.MeasureString(b.Text, tempFont).Width < 100)
+                        {
+                            break;
+                        }
+                        if (tempResSize == 300)
+                        {
+                            MessageBox.Show("while true in Res");
+                            break;
+                        }
+                        tempResSize++;
+                    }
+                    if (tempResSize < 10)
+                    {
+                        resFont = new Font(b.Font.FontFamily, 8);
+                    }
+                    else
+                    {
+                        resFont = new Font(b.Font.FontFamily, tempResSize - 2);
+                    }
+                }
+
+            }
+            else
+            {
+                resSize = tempSizeYInPoint;
+                resFont = new Font(b.Font.FontFamily, resSize);
+            }
+            //resSize = (resSize > 10) ? resSize - 1 : 8;
+            b.Font = resFont;
+
+
+        }
+        
+        private void run_Click(object sender, EventArgs e)
+        {
+            resizeFont(buttonText);
+            resizeFont(button1);
+            resizeFont(button2);
+            resizeFont(button3);
+            resizeFont(button4);
+            resizeFont(button5);
+            resizeFont(button6);
+            resizeFont(button7);
+            resizeFont(button8);
+            resizeFont(button9);
+        }
+    }
+}
