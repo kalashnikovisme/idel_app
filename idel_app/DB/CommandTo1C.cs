2429bbb5ff1ca5688a5bf3ab74bef2eb563639b5
Дмитрий Давыдов <haudvd@gmail.com>
2012-11-26 15:35:36 +0400

    Work with 1C 5. Add start elements of request product and provider

--- idel_app/DB/CommandTo1C.cs
+++ idel_app/DB/CommandTo1C.cs
@@ -1,9 +1,172 @@
 ﻿using System;
 using System.Text;
 using System.Reflection;
+using System.Collections.Generic;
+using V82;
 
 namespace idel_app.DB
 {
+    class RequestTo1C
+    {
+        // Метод для подключения к 1С
+        static public bool Connect1C(string filename, string user, string password, ref object v82Base, ref COMConnectorClass connector)
+        {
+            try
+            {
+                v82Base = connector.Connect(GetConnectionString(filename, user, password));
+                return true;
+            }
+            catch (Exception ex)
+            {
+                string error = ("Ошибка подключения!\n" + ex.Message);
+                return false;
+            }
+        }
+
+        // Метод для подключения к 1С
+        static public bool Connect1C(ref object v82Base, ref COMConnectorClass connector)
+        {
+            try
+            {
+                v82Base = connector.Connect(GetConnectionString(@"d:\programming\1c\идель\", "", ""));
+                return true;
+            }
+            catch (Exception ex)
+            {
+                string error = ("Ошибка подключения!\n" + ex.Message);
+                return false;
+            }
+        }
+
+        // Фомирует строку подключения
+        static private string GetConnectionString(string filename, string user, string password)
+        {
+            StringBuilder ConnectionString = new StringBuilder(100);
+            ConnectionString.Append(@"File=""" + filename + @""";");
+            ConnectionString.Append(@"Usr=" + (user == null ? @";" : @"""" + user + @""";"));
+            ConnectionString.Append(@"Pwd=" + (password == null ? @";" : @"""" + password + @""";"));
+            return ConnectionString.ToString();
+        }
+
+        //получает из базы значения и формирует лист листов заявок
+        static public List<List<string>> FillListListRequests(object v82Base, COMConnectorClass connector)
+        {
+            List<List<string>> list = new List<List<string>>();
+            List<string> id = new List<string>();
+            List<string> title = new List<string>();
+            List<string> createDate = new List<string>();
+            List<string> passDate = new List<string>();
+            List<string> employee = new List<string>();
+            List<string> product = new List<string>();
+            List<string> provider = new List<string>();
+            List<string> count = new List<string>();
+            List<string> wareHouseStatus = new List<string>();
+            List<string> requestStatus = new List<string>();
+            List<string> comment = new List<string>();
+            object storage = CommandTo1C.ExecuteCreateObject(v82Base, "NewObject", new object[] { "Запрос" });
+            CommandTo1C.SetProperty(storage, "Текст", new object[] { CommandTo1C.RequestIdel });
+            object result = CommandTo1C.ExecuteFunction(storage, "Выполнить", new object[] { });
+            object selection = CommandTo1C.ExecuteFunction(result, "Выбрать", null);
+            while ((bool)CommandTo1C.ExecuteFunction(selection, "Следующий", null))
+            {
+                id.Add((string)CommandTo1C.GetProperty(selection, "Артикул"));
+                title.Add("Заголовок заявки");
+                createDate.Add("12.12.2012 7:23:00");
+                passDate.Add("12.12.2012 7:23:00");//собственный парсер даты скорее всего нужен будет
+                employee.Add("сотрудник");
+                product.Add((string)CommandTo1C.GetProperty(selection, "НаименованиеТовара"));
+                provider.Add((string)CommandTo1C.GetProperty(selection, "ОсновнойПоставщик"));
+                count.Add("3");//(string)CommandTo1C.GetProperty(selection, "Количество"));
+                wareHouseStatus.Add("Истина");//будет по русски
+                requestStatus.Add("Ложь");//будеть по руски Ложь
+                comment.Add((string)CommandTo1C.GetProperty(selection, "Комментарий"));
+            }
+
+            try
+            {
+                for (int i = 0; i < id.Count; i++)
+                {
+                    list.Add(new List<string> {id[i], title[i], createDate[i], passDate[i], employee[i], product[i], provider[i], count[i], wareHouseStatus[i],
+                            requestStatus[i], comment[i]});
+                }
+            }
+            catch (Exception ex)
+            {
+                string error = ex.Message;
+            }
+            return list;
+        }
+
+        //получает из базы значения и формирует лист листов поставщиков
+        static public List<List<string>> FillListListProvider(object v82Base, COMConnectorClass connector)
+        {
+            List<List<string>> list = new List<List<string>>();
+            List<string> id = new List<string>();
+            List<string> title = new List<string>();
+            List<string> phone = new List<string>();
+            List<string> email = new List<string>();
+            object storage = CommandTo1C.ExecuteCreateObject(v82Base, "NewObject", new object[] { "Запрос" });
+            CommandTo1C.SetProperty(storage, "Текст", new object[] { CommandTo1C.RequestProvider });
+            object result = CommandTo1C.ExecuteFunction(storage, "Выполнить", new object[] { });
+            object selection = CommandTo1C.ExecuteFunction(result, "Выбрать", null);
+            while ((bool)CommandTo1C.ExecuteFunction(selection, "Следующий", null))
+            {
+                id.Add((string)CommandTo1C.GetProperty(selection, "Артикул"));
+                title.Add("Заголовок заявки");
+                phone.Add("+9637201212");//будеть по руски Ложь
+                email.Add("DarthVader@LordSith.com");
+            }
+
+            try
+            {
+                for (int i = 0; i < id.Count; i++)
+                {
+                    list.Add(new List<string> {id[i], title[i], phone[i], email[i]});
+                }
+            }
+            catch (Exception ex)
+            {
+                string error = ex.Message;
+            }
+
+            return list;
+        }
+
+        //получает из базы значения и формирует лист листов товаров
+        static public List<List<string>> FillListListProduct(object v82Base, COMConnectorClass connector)
+        {
+            List<List<string>> list = new List<List<string>>();
+            List<string> id = new List<string>();
+            List<string> title = new List<string>();
+            List<string> description = new List<string>();
+
+            object storage = CommandTo1C.ExecuteCreateObject(v82Base, "NewObject", new object[] { "Запрос" });
+            CommandTo1C.SetProperty(storage, "Текст", new object[] { CommandTo1C.RequestProduct });
+            object result = CommandTo1C.ExecuteFunction(storage, "Выполнить", new object[] { });
+            object selection = CommandTo1C.ExecuteFunction(result, "Выбрать", null);
+            while ((bool)CommandTo1C.ExecuteFunction(selection, "Следующий", null))
+            {
+                id.Add((string)CommandTo1C.GetProperty(selection, "Артикул"));
+                title.Add("Заголовок заявки");
+                description.Add("Это описание");
+            }
+
+            try
+            {
+                for (int i = 0; i < id.Count; i++)
+                {
+                    list.Add(new List<string> { id[i], title[i], description[i] });
+                }
+            }
+            catch (Exception ex)
+            {
+                string error = ex.Message;
+            }
+
+            return list;
+        }
+    }
+
     class CommandTo1C
     {
         /// <summary>
@@ -103,6 +266,17 @@ namespace idel_app.DB
 	                 ИЗ
 	                	Справочник.ТипыЦенНоменклатуры КАК ТипыЦенНоменклатуры";
         public static string RequestIdel = @"ВЫБРАТЬ 
+                     ТоварыНаСкладах.Номенклатура.Артикул КАК Артикул,
+                     ТоварыНаСкладах.Номенклатура.НаименованиеПолное КАК НаименованиеТовара,
+	               	 ТоварыНаСкладах.Номенклатура.Комментарий КАК Комментарий,
+	                 ТоварыНаСкладах.Номенклатура.ОсновнойПоставщик.Наименование КАК ОсновнойПоставщик
+	               ИЗ
+	                 РегистрНакопления.ТоварыНаСкладах КАК ТоварыНаСкладах";
+        public static string RequestProvider = @"ВЫБРАТЬ 
+                     Контрагенты.НаименованиеПолное КАК Наименование
+	               ИЗ
+	                 Справочник.Контрагенты КАК Поставщики";
+        public static string RequestProduct = @"ВЫБРАТЬ 
                      ТоварыНаСкладах.Номенклатура.Артикул КАК Артикул,
                      ТоварыНаСкладах.Номенклатура.НаименованиеПолное КАК НаименованиеТовара,
 	               	 ТоварыНаСкладах.Номенклатура.Комментарий КАК Комментарий,
--- idel_app/DB/Request_DB.cs
+++ idel_app/DB/Request_DB.cs
@@ -20,87 +20,13 @@ namespace idel_app.DB {
         // объект подключения к базе
         object v82Base = null;//мб лучше сделать полем класса
 
-        bool connect = Connect1C(GetConnectionString(), ref v82Base, ref connector);
+        bool connect = RequestTo1C.Connect1C(ref v82Base, ref connector);
 
-        List<List<string>> requests = FillListList(v82Base, connector);
+        List<List<string>> requests = RequestTo1C.FillListListRequests(v82Base, connector);
+
 
       return requests;
-    }
-
-    // Метод для подключения к 1С
-    static private bool Connect1C(string connectionString, ref object v82Base, ref COMConnectorClass connector)
-    {
-        try
-        {
-            v82Base = connector.Connect(connectionString);
-            return true;
-        }
-        catch (Exception ex)
-        {
-            string error = ("Ошибка подключения!\n" + ex.Message);
-            return false;
-        }
-    }
-
-    // Фомирует строку подключения
-    static private string GetConnectionString()
-    {
-        StringBuilder ConnectionString = new StringBuilder(100);
-        ConnectionString.Append(@"File=""" + @"d:\programming\1c\идель\"/*здесь путь к файлу*/ + @""";");
-        ConnectionString.Append(@"Usr=" + (""/*здесь имя пользователя*/ == null ? @";" : @"""" + ""/*здесь имя пользователя*/ + @""";"));
-        ConnectionString.Append(@"Pwd=" + (""/*здесь пароль*/ == null ? @";" : @"""" + ""/*здесь пароль*/ + @""";"));
-        return ConnectionString.ToString();
-    }
-
-    //получает из базы значения и формирует лист листов
-    static private List<List<string>> FillListList(object v82Base, COMConnectorClass connector)
-    {
-        List<List<string>> list = new List<List<string>>(); 
-         List<string> id = new List<string>();
-         List<string> title = new List<string>();
-         List<string> createDate = new List<string>();
-         List<string> passDate = new List<string>();
-         List<string> employee = new List<string>();
-         List<string> product = new List<string>();
-         List<string> provider = new List<string>();
-         List<string> count = new List<string>();
-         List<string> wareHouseStatus = new List<string>(); 
-         List<string> requestStatus = new List<string>();
-         List<string> comment = new List<string>();
-        object storage = CommandTo1C.ExecuteCreateObject(v82Base, "NewObject", new object[] { "Запрос" });
-        CommandTo1C.SetProperty(storage, "Текст", new object[] { CommandTo1C.RequestIdel });
-        object result = CommandTo1C.ExecuteFunction(storage, "Выполнить", new object[]{});
-        object selection = CommandTo1C.ExecuteFunction(result, "Выбрать", null);
-        while ((bool)CommandTo1C.ExecuteFunction(selection, "Следующий", null))
-        {
-            id.Add((string)CommandTo1C.GetProperty(selection, "Артикул"));
-            title.Add("Заголовок заявки");
-            createDate.Add("12.12.2012 7:23:00");
-            passDate.Add("12.12.2012 7:23:00");//собственный парсер даты скорее всего нужен будет
-            employee.Add("сотрудник");
-            product.Add((string)CommandTo1C.GetProperty(selection, "НаименованиеТовара"));
-            provider.Add((string)CommandTo1C.GetProperty(selection, "ОсновнойПоставщик"));
-            count.Add("3");//(string)CommandTo1C.GetProperty(selection, "Количество"));
-            wareHouseStatus.Add("true");//будет по русски
-            requestStatus.Add("false");//будеть по руски Ложь
-            comment.Add((string)CommandTo1C.GetProperty(selection, "Комментарий"));
-        }
-
-        try
-        {
-            for (int i = 0; i < id.Count; i++)
-            {
-                list.Add(new List<string> {id[i], title[i], createDate[i], passDate[i], employee[i], product[i], provider[i], count[i], wareHouseStatus[i],
-                            requestStatus[i], comment[i]});
-            }
-        }
-        catch(Exception ex)
-        {
-            string error = ex.Message;
-        }
-
-        return list;
-    }
+    }    
 
     static private int PInt(string s) {
       return Int32.Parse(s);
